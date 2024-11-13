ab601cd1481ee79a64c4e276b29788f08cdec0ba
keshav <keshavkumar584@gmail.com>
2024-11-13 23:40:44 +0530

    Vehicle assignment solution

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -9,12 +9,22 @@ public class Car extends Vehicle {
     private int currentGear;
     private int seats;
 
+
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
         //Hint: Car extends Vehicle
+        super(name);
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManual = isManual;
+        this.type = type;
+        this.seats = seats;
+        this.currentGear = 1;
+
     }
 
     public void changeGear(int newGear){
-
+        this.currentGear = newGear;
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
